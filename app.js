c1da55b6fdff79ccd9effc630c468a80defcab8d
mplouffe <matheuplouffe@gmail.com>
2018-08-07 22:31:17 -0500

    Set up to tweet on an 8 hour interval. Modified some of the text.

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ require('./config/config');
 const Twitter = require('twitter');
 const { generateTextTweet } = require('./text-tweet-generator/tweet-generator');
 
-const eightHours = 28800000;
-const fiveSeconds = 5000;
-
 const client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -12,19 +9,15 @@ const client = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-
-client.post('statuses/update', {status: 'Rnd Encounter is up and running!'})
-    .then((tweet) => {
-        console.log("Success");
-        console.log(tweet);
-    })
-    .catch((err) => {
-        console.log('Error!');
-        console.log(err);
-    });
-
-
 setInterval(() => {
-    const status = generateTextTweet();
-    client.post('status/update', { status })
-}, fiveSeconds);
+    generateTextTweet()
+        .then((status) => {
+            return client.post('statuses/update', { status: status });
+        })
+        .then((tweet) => {
+            console.log('Successful tweet!')
+        })
+        .catch((err) => {
+            console.log('ERR!: ', err);
+        });
+}, 28800000);
--- text-tweet-generator/tweet-generator.js
+++ text-tweet-generator/tweet-generator.js
@@ -1,27 +1,31 @@
 const fs = require('fs');
 
-export const generateTextTweet = () => {
+const generateTextTweet = () => {
     let tweetFormat = Math.floor(Math.random() * 9) + 1;
     let tweetPath = `./old-text/tweet_0${tweetFormat}.json`;
-    fs.readFile(tweetPath, 'utf8', (err, data) => {
-        if(err) {
-            console.log(err);
-            return;
-        }
-
-        let jsonData = JSON.parse(data);
-        let elements = jsonData.tweet.elements;
-
-        let tweetText = "";
-        elements.forEach(element => {
-            if(element.length === 1) {
-                tweetText += element[0];
-            } else {
-                let pickedIndex = Math.floor(Math.random() * element.length);
-                tweetText += element[pickedIndex];
+    return new Promise(function(resolve, reject) {
+        fs.readFile(tweetPath, 'utf8', (err, data) => {
+            if(err) {
+                reject(err);
+                return;
             }
+
+            let jsonData = JSON.parse(data);
+            let elements = jsonData.tweet.elements;
+
+            let tweetText = "";
+            elements.forEach(element => {
+                if(element.length === 1) {
+                    tweetText += element[0];
+                } else {
+                    let pickedIndex = Math.floor(Math.random() * element.length);
+                    tweetText += element[pickedIndex];
+                }
+            });
+
+            resolve(tweetText);
         });
-        return tweetText;
     });
 };
 
+module.exports = { generateTextTweet };
