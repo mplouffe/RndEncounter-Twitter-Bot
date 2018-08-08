5d270bc5901f4910cadb8a93967fc8cfc3e479b6
mplouffe <matheuplouffe@gmail.com>
2018-08-08 11:42:54 -0500

    Added simple express server to project for heroku deployment.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require('./config/config');
+const express = require('express');
 const Twitter = require('twitter');
 const { generateTextTweet } = require('./text-tweet-generator/tweet-generator');
+
+const app = express();
+const PORT = process.env.PORT;
 
 const client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -9,15 +13,25 @@ const client = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+client.post('statuses/update', {status: 'RndEncounter is up and running.'})
+    .then(() => {
+        console.log('Success!');
+    })
+    .catch((err) => {
+        console.log('ERR: ', err);
+    });
+
 setInterval(() => {
     generateTextTweet()
         .then((status) => {
             return client.post('statuses/update', { status: status });
         })
         .then((tweet) => {
-            console.log('Successful tweet!')
+            // console.log('Successful tweet!')
         })
         .catch((err) => {
             console.log('ERR!: ', err);
         });
 }, 28800000);
+
+app.listen(PORT, () => console.log(`Rnd Encounter up and running on ${PORT}`));
